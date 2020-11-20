2a5d391c2652512feb780e4396f3123f824ddf2b
Blanc Kévin <blanckevin19@gmail.com>
2020-11-20 16:05:16 +0100

    nav bar + backlog category

---
+++ src/Form/CategoryFormType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+
+class CategoryFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $category)
+    {
+        $builder
+            ->add('name', TextType::class)
+            
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Category::class,
+        ]);
+    }
+}
